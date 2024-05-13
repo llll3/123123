5b5fab31e3d03d176878061c7962df26278734bc
llll3 <90038489+llll3@users.noreply.github.com>
2024-05-13 23:47:29 +0300

    1.py

--- 1.py
+++ 1.py
@@ -1,16 +1,21 @@
 import telebot
 from telebot import types
-import re
 import time
+import re
+import threading
 from functools import wraps
 from cachetools import TTLCache
 
 bot = telebot.TeleBot('7018618926:AAEadbtQEiwryYeHCzc3SRsciEtIe0i55U4')
 authorized_ids = set()
+cache = TTLCache(maxsize=100, ttl=60)
 
+def edit_message(chat_id, message_id, new_text):
+    bot.edit_message_text(new_text, chat_id, message_id, parse_mode="html")
 
-# Создаем кеш с истечением срока действия через 1 минуту
-cache = TTLCache(maxsize=100, ttl=30)
+def replace_message(chat_id, message_id, new_text):
+    bot.delete_message(chat_id, message_id)
+    edit_message(chat_id, message_id, new_text)
 
 def authorized_only(func):
     @wraps(func)
@@ -19,239 +24,228 @@ def authorized_only(func):
         if cache.get(user_id):
             return func(*args, **kwargs)
         else:
-            bot.send_message(args[0].chat.id, "<b>Доступ запрещен.</b>", parse_mode="html")
+            msg = bot.send_message(args[0].chat.id, "<b>Доступ запрещен.</b>", parse_mode="html")
+            bot.delete_message(args[0].chat.id, args[0].message_id)
+            threading.Timer(2.0, bot.delete_message, (msg.chat.id, msg.message_id)).start()
             return
-
     return wrapper
+
+file_name = 'user_messages_count.txt'
+
+
+def get_user_message_count(chat_id):
+    with open(file_name, 'r') as f:
+        data = f.readlines()
+
+    for line in data:
+        if str(chat_id) in line:
+            return int(line.split(':')[1])
+    return 0
+
+
+def update_user_message_count(chat_id, message_count):
+    with open(file_name, 'r') as f:
+        data = f.readlines()
+
+    with open(file_name, 'w') as f:
+        updated = False
+        for line in data:
+            if str(chat_id) in line:
+                f.write(f"{chat_id}:{message_count}\n")
+                updated = True
+            else:
+                f.write(line)
+
+        if not updated:
+            f.write(f"{chat_id}:{message_count}\n")
+
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    # Загружаем список администраторов из файла
+    # Получение количества отправленных сообщений пользователем
+    message_count = get_user_message_count(message.chat.id)
+
+    # Вывод количества отправленных сообщений в консоль
+    print(f"Пользователь {message.from_user.first_name} отправил {message_count} сообщений.")
+
+    # Увеличение счетчика сообщений пользователя
+    update_user_message_count(message.chat.id, message_count + 1)
+
+    # Приветственное сообщение
+    if message_count == 0:
+        bot.send_message(message.chat.id, f"<b>Добро пожаловать, <a href='tg://user?id={message.from_user.id}'>\
+{message.from_user.first_name}</a>!</b>", parse_mode="html")
+    admins = set()
+    bot.delete_message(message.chat.id, message.message_id)  # Удаление сообщения
     with open("Admins.txt", "r") as f:
-        authorized_ids = [int(line.strip()) for line in f]
-
-    # Добавляем список администраторов в кеш
-    for admin_id in authorized_ids:
+        admins = {int(line.strip()) for line in f}
+    for admin_id in admins:
         cache[admin_id] = True
-
     user_id = message.from_user.id
     if not cache.get(user_id):
-        bot.send_message(message.chat.id, "<b>Доступ запрещен.</b>", parse_mode="html")
+        msg2 = bot.send_message(message.chat.id, "<b>Доступ запрещен.</b>", parse_mode="html")
+        threading.Timer(2.0, bot.delete_message, (msg2.chat.id, msg2.message_id)).start()
         return
-
-    # Создаем клавиатуру
-    keyboard = telebot.types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    button_link = telebot.types.KeyboardButton("Ссылка")
-    button_stats = telebot.types.KeyboardButton("Статистика")
-    button_adminss = telebot.types.KeyboardButton("Администраторы")
-    keyboard.add(button_link, button_stats, button_adminss)
-
-    # Отправляем клавиатуру пользователю
-    bot.send_message(message.chat.id, "<b>Выберите действие:</b>", reply_markup=keyboard, parse_mode="html")
-
-@bot.message_handler(func=lambda message: message.text == "Администраторы")
-@authorized_only
-
-def adminss(message):
-
-    with open("Admins.txt", "r") as f:
-        admins = [line.strip() for line in f]
-
-    admins_str = "<b>Список Администраторов:</b>\n\n"
-    count = 1
-    for admin in admins:
-        admins_str += f"<b>[{count}] <a href='tg://user?id={admin}'>{admin}</a>\n</b>"  # Изменена строка для добавления гиперссылки на профиль пользователя
-        count += 1
-
-    time.sleep(0.5)  # Добавлена задержка в 500 мс
-    bot.send_message(message.chat.id, admins_str, parse_mode="html")
-
-    # Создаем клавиатуру
-    keyboard = telebot.types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    button_add = telebot.types.KeyboardButton("Добавить")
-    button_del = telebot.types.KeyboardButton("Удалить")
-    button_back = telebot.types.KeyboardButton("Назад")
-    keyboard.add(button_add, button_del, button_back)
-
-    # Отправляем клавиатуру пользователю
-    bot.send_message(message.chat.id, "<b>Выберите действие:</b>", reply_markup=keyboard, parse_mode="html")
-
-@bot.message_handler(func=lambda message: message.text == "Добавить")
-@authorized_only
-def add_admin(message):
-
-    # Очищаем клавиатуру
-    keyboard = telebot.types.ReplyKeyboardRemove()
-    bot.send_message(message.chat.id, "<b>Введите ID нового администратора:</b>", reply_markup=keyboard, parse_mode="html")
-    bot.register_next_step_handler(message, add_admin)
-
-@authorized_only
-def add_admin(message):
-    admin_id = message.text
-
-    # Проверяем, что введенный текст содержит только цифры
-    if not re.match("^[0-9]+$", admin_id):
-        bot.send_message(message.chat.id, "<b>ID администратора должен содержать только цифры</b>", parse_mode="html")
-        return  # Прерываем выполнение функции
-
-    with open("Admins.txt", "a") as f:
-        f.write("\n" + admin_id)
-
-    f.close()
-
-    authorized_ids.add(int(admin_id))
-
-    bot.send_message(message.chat.id, "<b>Успешно добавлено!</b>", parse_mode="html")
-    adminss(message)
-
-
-@bot.message_handler(func=lambda message: message.text == "Удалить")
-@authorized_only
-def del_admin(message):
-
-    # Очищаем клавиатуру
-    keyboard = telebot.types.ReplyKeyboardRemove()
-    bot.send_message(message.chat.id, "<b>Введите порядковый номер администратора для удаления:</b>", reply_markup=keyboard, parse_mode="html")
-    bot.register_next_step_handler(message, del_admin)
-
-@authorized_only
-def del_admin(message):
-    admin_number = message.text
-
-    # Проверяем, что введенный текст содержит только цифры
-    if not re.match("^[0-9]+$", admin_number):
-        bot.send_message(message.chat.id, "<b>Порядковый номер администратора должен содержать только цифры.</b>", parse_mode="html")
-        return  # Прерываем выполнение функции
-
-    admins = []
-    with open("Admins.txt", "r") as f:
-        admins = [line.strip() for line in f]
-
-    # Преобразуем порядковый номер в индекс
-    index = int(admin_number) - 1
-
-    # Проверяем, что введенный порядковый номер корректен
-    if index < 0 or index >= len(admins):
-        bot.send_message(message.chat.id, "<b>Некорректный порядковый номер администратора</b>", parse_mode="html")
-        return
-
-    admin_id = admins[index]
-
-    # Удаляем администратора из списка
-    admins.pop(index)
-    #if admins:  # Проверка, не пуст ли список
-    #    admins[index - 1] += "\n"
-    with open("Admins.txt", "w") as f:
-        f.write("\n".join(admins))
-
-    bot.send_message(message.chat.id, "<b>Успешно удалено!</b>", parse_mode="html")
-    f.close()
-    adminss(message)
-
-
-@bot.message_handler(func=lambda message: message.text == "Назад")
-def back(message):
-    start(message)
-
-@bot.message_handler(func=lambda message: message.text == "Статистика")
-@authorized_only
-def stats(message):
-    # Считываем данные из файла Users.txt
-    with open('Users.txt', 'r') as f:
-        lines = f.readlines()
-
-    # Инициализируем счетчики для каждого периода времени
-    last_month_users = 0
-    current_month_users = 0
-    current_week_users = 0
-    current_day_users = 0
-
-    # Обрабатываем каждую строку в файле и обновляем счетчики
-    for line in lines:
-        user_id, timestamp = line.split(' - ')
-        timestamp = float(timestamp.split('.')[0])
-
-        # Определяем период времени, к которому относится запись
-        if timestamp > time.time() - 60 * 60 * 24:  # меньше дня назад
-            current_day_users += 1
-        elif timestamp > time.time() - 60 * 60 * 24 * 7:  # меньше недели назад
-            current_week_users += 1
-        elif timestamp > time.time() - 60 * 60 * 24 * 30:  # меньше месяца назад
-            current_month_users += 1
-        else:  # больше месяца назад
-            last_month_users += 1
-
-    # Создаем клавиатуру
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    button_back = types.KeyboardButton("Назад")
-    keyboard.add(button_back)
+    buttons = [types.KeyboardButton(btn) for btn in ["Ссылка", "Статистика", "Администраторы"]]
+    keyboard.add(*buttons)
 
-    msg1 = bot.send_message(message.chat.id, "<b>Статистика:</b>", reply_markup=keyboard, parse_mode="html")
+    mm = bot.send_message(message.chat.id, "<b>Выберите действие:</b>", reply_markup=keyboard, parse_mode="html",
+                          disable_notification=True)
 
-    # Выводим метрики
-    bot.send_message(message.chat.id, f"<b>Количество пользователей:\n"
-                                      f"\nЗа прошлый месяц: {last_month_users}"
-                                      f"\nЗа текущий месяц: {current_month_users}"
-                                      f"\nЗа текущую неделю: {current_week_users}"
-                                      f"\nЗа текущий день: {current_day_users}</b>", parse_mode="html")
-
-    bot.register_next_step_handler(msg1, back)
-
-@authorized_only
-def back(message):
-    if message.text == "Назад":
-        start(message)
-    else:
-        bot.send_message(message.chat.id, "<b>Неверный выбор. Повторите попытку.</b>", parse_mode="html")
-        link(message)
-
+    time.sleep(2.0)
+    bot.delete_message(mm.chat.id, mm.message_id)
 
 @bot.message_handler(func=lambda message: message.text == "Ссылка")
 @authorized_only
 def link(message):
-    # Читаем файл Link_edit.txt
-    with open('Link_edit.txt', 'r') as f:
-        file_content = f.read()
-
-    # Извлекаем старую ссылку
-    old_link = file_content.split()[0]
-
-    # Создаем клавиатуру
+    global msg_link
+    link_edit = open('Link_edit.txt', 'r').read()
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    button_new_link = types.KeyboardButton("Изменить ссылку")
-    button_back = types.KeyboardButton("Назад")
-    keyboard.add(button_new_link, button_back)
+    buttons = [types.KeyboardButton(btn) for btn in ["Изменить ссылку", "Назад"]]
+    keyboard.add(*buttons)
+    bot.delete_message(message.chat.id, message.message_id)
+    # Удаляем HTML-разметку из текста сообщения
+    link_edit_without_html = link_edit.replace("<b>", "").replace("</b>", "")
+    msg_link = bot.send_message(message.chat.id, f"Текущая ссылка: {link_edit_without_html}", reply_markup=keyboard)
+    #bot.register_next_step_handler(msg_link, back_link, handle_link_action)
 
-    # Отправляем пользователю сообщение со старой ссылкой и клавиатурой
-    msg = bot.send_message(message.chat.id, f"<b>Текущая ссылка на папку: {old_link}\n\nВыберите действие:</b>",
-                           reply_markup=keyboard, disable_web_page_preview=True, parse_mode="html")
-    bot.register_next_step_handler(msg, handle_link_action)
-
+@bot.message_handler(func=lambda message: message.text == "Изменить ссылку")
 @authorized_only
 def handle_link_action(message):
     if message.text == "Изменить ссылку":
-        new_link(message, reply_markup=types.ReplyKeyboardRemove())
-    elif message.text == "Назад":
-        start(message)
-    else:
-        bot.send_message(message.chat.id, "<b>Неверный выбор. Повторите попытку.</b>", parse_mode="html")
-        link(message)
+        bot.delete_message(message.chat.id, message.message_id)
+        bot.delete_message(message.chat.id, msg_link.message_id)  # Удаляем сообщение со ссылкой
+        msg_link2 = bot.send_message(message.chat.id, "<b>Введите новую ссылку:</b>", parse_mode="html", reply_markup=types.ReplyKeyboardRemove())
+        threading.Timer(2.0, bot.delete_message, (msg_link2.chat.id, msg_link2.message_id)).start()
+        bot.register_next_step_handler(msg_link2, process_new_link)
+
+@bot.message_handler(func=lambda message: message.text == "Администраторы")
+@authorized_only
+def adminss(message):
+    global msg_admm
+    with open("Admins.txt", "r") as f:
+        admins = [line.strip() for line in f]
+    admins_str = "<b>Список Администраторов:</b>\n\n"
+    count = 1
+    for admin in admins:
+        admins_str += f"<b>[{count}] <a href='tg://user?id={admin}'>{admin}</a>\n</b>"
+        count += 1
+    time.sleep(0.5)  # Добавлена задержка в 500 мс
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    buttons = [types.KeyboardButton(btn) for btn in ["Добавить", "Удалить", "Назад"]]
+    keyboard.add(*buttons)
+    msg_admm = bot.send_message(message.chat.id, admins_str, reply_markup=keyboard, parse_mode="html")
+    bot.delete_message(message.chat.id, message.message_id)
+    #bot.register_next_step_handler(msg_admm, back_admm)
+
+@bot.message_handler(func=lambda message: message.text == "Добавить")
+@authorized_only
+def add_admin(message):
+    bot.delete_message(message.chat.id, msg_admm.message_id)
+    keyboard = types.ReplyKeyboardRemove()
+    msg_adm4 = bot.send_message(message.chat.id, "<b>Введите ID нового администратора:</b>", reply_markup=keyboard, parse_mode="html")
+    threading.Timer(2.0, bot.delete_message, (msg_adm4.chat.id, msg_adm4.message_id)).start()
+    bot.register_next_step_handler(message, add_admin)
+    bot.delete_message(message.chat.id, message.message_id)
 
 @authorized_only
-def new_link(message, reply_markup=None):
-    msg = bot.send_message(message.chat.id, "<b>Введите новую ссылку:</b>", parse_mode="html",
-                           reply_markup=reply_markup)
-    bot.register_next_step_handler(msg, process_new_link)
+def add_admin(message):
+    admin_id = message.text
+    if not re.match("^[0-9]+$", admin_id):
+        msg_adm6 = bot.send_message(message.chat.id, "<b>ID администратора должен содержать только цифры</b>", parse_mode="html")
+        threading.Timer(2.0, bot.delete_message, (msg_adm6.chat.id, msg_adm6.message_id)).start()
+        return
+    with open("Admins.txt", "a") as f:
+        f.write("\n" + admin_id)
+    f.close()
+    authorized_ids.add(int(admin_id))
+    msg_adm5 = bot.send_message(message.chat.id, "<b>Успешно добавлено!</b>", parse_mode="html")
+    threading.Timer(2.0, bot.delete_message, (msg_adm5.chat.id, msg_adm5.message_id)).start()
+    adminss(message)
+
+@bot.message_handler(func=lambda message: message.text == "Удалить")
+@authorized_only
+def del_admin(message):
+    keyboard = types.ReplyKeyboardRemove()
+    msg_adm3 = bot.send_message(message.chat.id, "<b>Введите порядковый номер администратора для удаления:</b>", reply_markup=keyboard, parse_mode="html")
+    threading.Timer(2.0, bot.delete_message, (msg_adm3.chat.id, msg_adm3.message_id)).start()
+    bot.register_next_step_handler(message, del_admin)
+    bot.delete_message(message.chat.id, message.message_id)
+
+@authorized_only
+def del_admin(message):
+    admin_number = message.text
+    if not re.match("^[0-9]+$", admin_number):
+        msg_adm2 = bot.send_message(message.chat.id, "<b>Порядковый номер администратора должен содержать только цифры.</b>", parse_mode="html")
+        threading.Timer(2.0, bot.delete_message, (msg_adm2.chat.id, msg_adm2.message_id)).start()
+        return
+    admins = []
+    with open("Admins.txt", "r") as f:
+        admins = [line.strip() for line in f]
+    index = int(admin_number) - 1
+    if index < 0 or index >= len(admins):
+        msg_adm1 = bot.send_message(message.chat.id, "<b>Некорректный порядковый номер администратора</b>", parse_mode="html")
+        threading.Timer(2.0, bot.delete_message, (msg_adm1.chat.id, msg_adm1.message_id)).start()
+        return
+    admin_id = admins[index]
+    admins.pop(index)
+    with open("Admins.txt", "w") as f:
+        f.write("\n".join(admins))
+    bot.send_message(message.chat.id, "<b>Успешно удалено!</b>", parse_mode="html")
+    bot.delete_message(message.chat.id, msg_admm.message_id)
+    f.close()
+    adminss(message)
+
+@bot.message_handler(func=lambda message: message.text == "Назад")
+def back_admm(message):
+    bot.delete_message(message.chat.id, msg_admm.message_id)
+    start(message)
+
+def back_link(message):
+        bot.delete_message(message.chat.id, msg_link.message_id)
+        start(message)
+def back_stats(message):
+        bot.delete_message(message.chat.id, msg_stats1.message_id)
+        start(message)
+
+@bot.message_handler(func=lambda message: message.text == "Статистика")
+@authorized_only
+def stats(message):
+    global msg_stats1
+    with open('Users.txt', 'r') as f:
+        lines = f.readlines()
+    stats = {"last_month": 0, "current_month": 0, "current_week": 0, "current_day": 0}
+    for line in lines:
+        user_id, timestamp = line.split(' - ')
+        timestamp = float(timestamp.split('.')[0])
+        if timestamp > time.time() - 60 * 60 * 24:
+            stats["current_day"] += 1
+        elif timestamp > time.time() - 60 * 60 * 24 * 7:
+            stats["current_week"] += 1
+        elif timestamp > time.time() - 60 * 60 * 24 * 30:
+            stats["current_month"] += 1
+        else:
+            stats["last_month"] += 1
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button_back = types.KeyboardButton("Назад")
+    keyboard.add(button_back)
+    bot.delete_message(message.chat.id, message.message_id)
+    msg_stats1 = bot.send_message(message.chat.id, f"<b>Количество пользователей:\n"
+                                      f"\nЗа прошлый месяц: {stats['last_month']}"
+                                      f"\nЗа текущий месяц: {stats['current_month']}"
+                                      f"\nЗа текущую неделю: {stats['current_week']}"
+                                      f"\nЗа текущий день: {stats['current_day']}</b>", reply_markup=keyboard, parse_mode="html")
+    bot.register_next_step_handler(msg_stats1, back_stats)
 
 @authorized_only
 def process_new_link(message):
     new_link = message.text
-
     with open('Link_edit.txt', 'w') as f:
         f.write(new_link)
-
-    bot.send_message(message.chat.id, "<b>Ссылка успешно обновлена!</b>", parse_mode="html")
+    msg_link3 = bot.send_message(message.chat.id, "<b>Ссылка успешно обновлена!</b>", parse_mode="html")
+    threading.Timer(2.0, bot.delete_message, (msg_link3.chat.id, msg_link3.message_id)).start()
     start(message)
-
 
 bot.infinity_polling()
